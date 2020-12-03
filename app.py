ea330ffd95d2d12b160a14a5f44ff4173c1f61ea
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-03 11:58:27 -0800

    Implemented routes

--- app.py
+++ app.py
@@ -21,3 +21,32 @@ def show_index():
         title=survey.title,
         instructions=survey.instructions
     )
+
+
+@app.route("/questions/<q_id>")
+def show_question(q_id):
+    """Shows a question in the survery
+    for a given question id number. """
+    q = survey.questions[int(q_id)]
+    return render_template("question.html",
+                           q_id=q_id,
+                           question=q.question,
+                           choices=q.choices)
+
+
+@app.route("/answer/<q_id>", methods=["POST"])
+def save_answer_and_redirect(q_id):
+    """ Saves the answer to the answer to responses list
+    and redirects the user. """
+
+    redirect_id = int(q_id) + 1
+    if redirect_id >= len(survey.questions):
+        return redirect("/thankyou")
+    else:
+        responses.append(request.form.get("answer"))
+        return redirect(f"/questions/{redirect_id}")
+
+
+@app.route("/thankyou")
+def show_thanks():
+    return render_template("completion.html")
