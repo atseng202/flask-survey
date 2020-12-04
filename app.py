58f4cf06c293d40c695dce7cd63f6d88fc4208b9
Alan Tseng <atseng202@gmail.com>
2020-12-03 17:04:46 -0800

    Refactor unnecessary session list for answers

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
-# Will replace with session
-responses = []
-
+RESPONSES_KEY = "responses"
 
 @app.route("/")
 def show_index():
     """Shows the user the title of survey,
     instructions, and button to start survey"""
 
-    session["responses"] = []
-    session["answered"] = []
+    session[RESPONSES_KEY] = []
 
     return render_template(
         "survey_start.html", title=survey.title,
@@ -27,54 +24,52 @@ def show_index():
 
 @app.route("/questions/<int:q_id>")
 def show_question(q_id):
-    """Shows a question in the survery
+    """Shows a question in the survey
     for a given question id number."""
 
-    if len(session["answered"]) == 0 and q_id != 0:
+    # No questions have been answered yet and user was browsing out of order
+    if len(session[RESPONSES_KEY]) == 0 and q_id != 0:
         q_id = 0
         return redirect("/questions/0")
 
-    if ((q_id - 1 in session["answered"] and q_id not in session["answered"])
-            or (q_id == 0 and 0 not in session["answered"])):
+    # This is a valid question to show as it is in order
+    if q_id == len(session[RESPONSES_KEY]):
         q = survey.questions[q_id]
         return render_template(
             "question.html", q_id=q_id, question=q.question, choices=q.choices
         )
     else:
         # check if the last question has been answered
-        last_answered_id = session["answered"][-1]
-        final_question_id = len(survey.questions)-1
-        if final_question_id in session["answered"]:
+        if len(session[RESPONSES_KEY]) == len(survey.questions):
             return redirect("/thankyou")
         else:
-            next_q_id = last_answered_id + 1
-            q = survey.questions[next_q_id]
+            next_q_id = len(session[RESPONSES_KEY])
             flash("Please answer questions in order!")
             return redirect(f"/questions/{next_q_id}")
 
 
 @app.route("/answer/<int:q_id>", methods=["POST"])
 def save_answer_and_redirect(q_id):
-    """Saves the answer to the answer to responses list
-    and redirects the user."""
+    """Saves the answer to RESPONSES_KEY list and redirects the user."""
 
-    save_to_session("answered", q_id)
-    save_to_session("responses", request.form.get("answer"))
+    append_to_session(RESPONSES_KEY, request.form.get("answer"))
 
-    redirect_id = q_id + 1
-    if redirect_id >= len(survey.questions):
+    if len(session[RESPONSES_KEY]) == len(survey.questions):
         return redirect("/thankyou")
     else:
-        return redirect(f"/questions/{redirect_id}")
+        return redirect(f"/questions/{q_id + 1}")
 
 
 @app.route("/thankyou")
 def show_thanks():
+    """ Shows thank you page once user has 
+    completed all questions of survey """
+
     return render_template("completion.html")
 
 
-def save_to_session(key, value):
-    """ Helper function for rebinding session responses """
+def append_to_session(key, value):
+    """ Helper function for rebinding session for RESPONSES_KEY"""
     data_list = session[key]
     data_list.append(value)
     session[key] = data_list
