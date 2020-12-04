01b57330747468e715a25e71a24a712d4e95c31d
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-03 16:33:22 -0800

    done!

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def show_index():
     session["answered"] = []
 
     return render_template(
-        "survey_start.html", title=survey.title, instructions=survey.instructions
-    )
+        "survey_start.html", title=survey.title,
+        instructions=survey.instructions)
 
 
 @app.route("/questions/<int:q_id>")
@@ -30,13 +30,12 @@ def show_question(q_id):
     """Shows a question in the survery
     for a given question id number."""
 
-    if len(session["answered"]) == 0:
-        q = survey.questions[0]
-        return render_template(
-            "question.html", q_id=0, question=q.question, choices=q.choices
-        )
+    if len(session["answered"]) == 0 and q_id != 0:
+        q_id = 0
+        return redirect("/questions/0")
 
-    if q_id - 1 in session["answered"] and q_id not in session["answered"]:
+    if ((q_id - 1 in session["answered"] and q_id not in session["answered"])
+            or (q_id == 0 and 0 not in session["answered"])):
         q = survey.questions[q_id]
         return render_template(
             "question.html", q_id=q_id, question=q.question, choices=q.choices
@@ -50,6 +49,7 @@ def show_question(q_id):
         else:
             next_q_id = last_answered_id + 1
             q = survey.questions[next_q_id]
+            flash("Please answer questions in order!")
             return redirect(f"/questions/{next_q_id}")
 
 
